d305b1a6bdf0102f4ad41c1500e78695c7035eec
zaferman <zaferman@btlab.ru>
2019-11-23 11:20:13 +0300

    До интеграции с CodeIgniter

--- src/componentns/ProductForm.js
+++ src/componentns/ProductForm.js
@@ -1,39 +1,92 @@
 import React from 'react';
-import ProductTable from "./ProductTable";
-import TextField from './TextField';
-import style from "../style/style";
+import {Row, Form, Col, Button} from 'react-bootstrap';
 
 class ProductForm extends React.Component {
-    state = {
-        name: "",
-        price: "",
+    constructor(props) {
+        super(props);
+        this.initialState = {
+            id: '',
+            productName: '',
+            price: '',
+            sku: ''
+        }
 
-    };
+        if (props.product) {
+            this.state = props.product
+        } else {
+            this.state = this.initialState;
+        }
 
-    onNameChange = event =>
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+
         this.setState({
-            name: event.target.value
-        });
+            [name]: value
+        })
+    }
 
-    onPriceChange = event =>
-        this.setState({
-            price: event.target.value
-        });
+    handleSubmit(event) {
+        event.preventDefault();
+        this.props.onFormSubmit(this.state);
+        this.setState(this.initialState);
+    }
 
     render() {
+
+        let pageTitle;
+        if (this.state.id) {
+            pageTitle = <h2>Edit Product</h2>
+        } else {
+            pageTitle = <h2>Add Product</h2>
+        }
+
         return (
             <div>
-                <div style={style.form}>
-                    <TextField label="Name" onChange={this.onNameChange} name='name'/>
-                    <TextField label="Price" onChange={this.onPriceChange} name='price'/>
-                </div>
-                <div>
-                    <ProductTable name={this.state.name} price={this.state.price}/>
-                </div>
+                {pageTitle}
+                <Row>
+                    <Col sm={6}>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Group controlId="productName">
+                                <Form.Label>Product Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="productName"
+                                    value={this.state.productName}
+                                    onChange={this.handleChange}
+                                    placeholder="Product Name"/>
+                            </Form.Group>
+                            <Form.Group controlId="sku">
+                                <Form.Label>SKU</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="sku"
+                                    value={this.state.sku}
+                                    onChange={this.handleChange}
+                                    placeholder="SKU"/>
+                            </Form.Group>
+                            <Form.Group controlId="price">
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="price"
+                                    value={this.state.price}
+                                    onChange={this.handleChange}
+                                    placeholder="Price"/>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Control type="hidden" name="id" value={this.state.id}/>
+                                <Button variant="success" type="submit">Save</Button>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+                </Row>
             </div>
-
-
-        );
+        )
     }
 
 }
--- src/componentns/ProductTable.js
+++ src/componentns/ProductTable.js
@@ -1,7 +1,6 @@
-import React, {Component} from "react";
+import React from "react";
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
-
-
+import {Table, Button, Alert} from 'react-bootstrap';
 var products = [{
     id: 1,
     name: "Product1",
@@ -12,21 +11,117 @@ var products = [{
     price: 80
 }];
 
-class ProductTable extends Component {
-    render() {
-        return (
-            <div>
-                <BootstrapTable data={products} striped hover>
-                    <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
-                    <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-                    <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-                </BootstrapTable>,
-                document.getElementById('basic')
-            </div>
-
-
-        );
+class ProductTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            products: products,
+            response: {}
+        }
     }
+
+    componentDidMount() {
+        // const apiUrl = 'http://localhost/dev/tcxapp/reactapi/products';
+
+        // fetch(apiUrl)
+        //     .then(res => res.json())
+        //     .then(
+        //         (result) => {
+        //             this.setState({
+        //                 products: result
+        //             });
+        //         },
+        //         (error) => {
+        //             this.setState({ error });
+        //         }
+        //     )
+    }
+
+    deleteProduct(productId) {
+        const {products} = this.state;
+
+        const apiUrl = 'http://localhost/dev/tcxapp/reactapi/deleteProduct';
+        const formData = new FormData();
+        formData.append('productId', productId);
+
+        const options = {
+            method: 'POST',
+            body: formData
+        }
+
+        fetch(apiUrl, options)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        response: result,
+                        products: products.filter(product => product.id !== productId)
+                    });
+                },
+                (error) => {
+                    this.setState({error});
+                }
+            )
+    }
+    render() {
+        const {error, products} = this.state;
+
+        if (error) {
+            return (
+                <div>Error: {error.message}</div>
+            )
+        } else {
+            return (
+                <div>
+                    <h2>Product List</h2>
+                    {this.state.response.message && <Alert variant="info">{this.state.response.message}</Alert>}
+                    <Table>
+                        <thead>
+                        <tr>
+                            <th>#ID</th>
+                            <th>Product Name</th>
+                            <th>SKU</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {products.map(product => (
+                            <tr key={product.id}>
+                                <td>{product.id}</td>
+                                <td>{product.name}</td>
+                                <td>{product.sku}</td>
+                                <td>{product.price}</td>
+                                <td>
+                                    <Button variant="info"
+                                            onClick={() => this.props.editProduct(product.id)}>Edit</Button>
+                                    &nbsp;<Button variant="danger"
+                                                  onClick={() => this.deleteProduct(product.id)}>Delete</Button>
+                                </td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </Table>
+                </div>
+            )
+        }
+    }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <BootstrapTable data={products} striped hover>
+    //                 <TableHeaderColumn isKey dataField='id'>Product ID</TableHeaderColumn>
+    //                 <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+    //                 <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+    //             </BootstrapTable>,
+    //             document.getElementById('basic')
+    //         </div>
+    //
+    //
+    //     );
+    // }
 }
 
 export default ProductTable;
